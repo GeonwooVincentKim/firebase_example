24a1f0c722898cbea671d34a233ef3f09f3a839c
GeonwooVincentKim <kdsnop@gmail.com>
2023-02-15 17:10:59 +0900

    [Feature] - Firebase with Flutter

--- lib/example/firebase_setup_main.dart
+++ lib/example/firebase_setup_main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_first_example/example/ex_screens/ex_home.dart';
+import 'package:flutter_firebase_first_example/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android
+  );
 
   runApp(MyApp());
 }
