5cbeb5a9e877910866ada2030724bda9308100cd
GeonwooVincentKim <kdsnop@gmail.com>
2023-02-16 16:27:20 +0900

    [Feature] - Main.dart has just solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_first_example/screens/login.dart';
+import 'package:flutter_firebase_first_example/old_firebase_options.dart';
+import 'package:flutter_firebase_first_example/screens/main_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android
+  );
 
   runApp(const MyApp());
 }
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false, 
-      home: LoginPage()
+      home: MainPage()
     );
   }
 }
